03ce32e6e5253edf8c9efd08c6f17328e882d9a5
xingjiudong <25635680@qq.com>
2017-08-15 17:30:14 +0800

    Add etcd config

--- create-mongodb.sh
+++ create-mongodb.sh
@@ -3,4 +3,17 @@ set -e
 
 source ./env.config
 
+#Env Information
+CLUSTER_PATH=$1
+: ${KEYFILE:=/etc/keyfile/mongodb-keyfile}
+
+#Created Mongodb_Keyfile
+sudo mkdir -p /etc/keyfile
+sudo openssl rand -base64 741 > ${KEYFILE}
+sudo chmod 600 ${KEYFILE}
+sudo chown 999 ${KEYFILE}
+
+#Create Etcd For Mongo
+etcdctl set ${CLUSTER_PATH:-/mongo-config}/mongodb-keyfile < ${KEYFILE}
+
 docker-compose ${COMPOSE_OPT} up -d
