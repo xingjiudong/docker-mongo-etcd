aff247fd926b4d9f30af7b3f8b6bd9836dce1a29
xingjiudong <25635680@qq.com>
2017-08-24 11:25:04 +0800

    add haproxy for mongo

--- initiate.sh
+++ initiate.sh
@@ -11,7 +11,6 @@ source ./env.config
 docker-compose ${COMPOSE_OPT} exec rs0node1 bash -c "echo 'rs.initiate();sleep(1000);' | mongo admin"
 docker-compose ${COMPOSE_OPT} exec rs0node1 bash -c "echo 'db.createUser({user: \"${USERNAME}\", pwd: \"${PASSWORD}\", roles: [ { role: \"root\", db: \"admin\" } ] });' | mongo admin"
 
-
 echo "Intializing replica rs0 set"
 replicate="cfg = rs.conf(); cfg.members[0].host = \"${REPLSET_NAME}node1\"; rs.reconfig(cfg); rs.add(\"${REPLSET_NAME}node2\"); rs.add(\"${REPLSET_NAME}node3\"); rs.status();"
 docker-compose ${COMPOSE_OPT} exec ${REPLSET_NAME}node1 bash -c "echo '${replicate}' | mongo  -u ${USERNAME} -p ${PASSWORD} --authenticationDatabase admin"
