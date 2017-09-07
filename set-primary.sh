9626b8958599039d2e589c8527ff4ac6a4cf5ac6
xingjiudong <25635680@qq.com>
2017-09-07 14:24:40 +0800

    Fix haproxy-etcd

--- set-primary.sh
+++ set-primary.sh
@@ -3,16 +3,9 @@ set -e
 source ./env.config
 
 #Get mongodb replset primary node;
-SERVER_NAME=` docker-compose ${COMPOSE_OPT} $@ exec rs0node1 bash -c \
+PRIMARY_NODE=` docker-compose ${COMPOSE_OPT} $@ exec rs0node1 bash -c \
          "echo 'rs.status().members.find(r=>r.state===1).name;' | mongo  -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin" \
-         | grep rs0 \
-         | sed 's/:[0-9][0-9][0-9][0-9][0-9]//g'`
-
-SERVER_PORT=` docker-compose ${COMPOSE_OPT} $@ exec rs0node1 bash -c \
-         "echo 'rs.status().members.find(r=>r.state===1).name;' | mongo  -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin" \
-         | grep rs0 \
-         | sed 's/.*://g'` 
+         | grep rs0` 
 
 #Set etcd for mongodb replset primary node;
-etcdctl set /haproxy-config/mongo/server_name ${SERVER_NAME}
-etcdctl set /haproxy-config/mongo/server_port ${SERVER_PORT}
+etcdctl set etcdctl set haproxy-config/${PROJECT_NAME}/mongo/backend/mongo-1 ${PRIMARY_NODE}
