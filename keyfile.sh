91c63e2c4706e3dbc832f0d9f3decd4ad19c5ca1
thinkernel <thinkernel@gmail.com>
2017-09-18 16:07:09 +0800

    change temp file location

--- keyfile.sh
+++ keyfile.sh
@@ -5,9 +5,8 @@ set -e
 REPLSET_NAME=$1
 
 #Created Mongodb-Keyfile
-openssl rand -base64 741 > /mongodb-keyfile
-chmod 600 /mongodb-keyfile 
-chown 999 /mongodb-keyfile
+openssl rand -base64 741 > ./mongodb-keyfile
 
 #Create Etcd For Mongo
-etcdctl set /mongo-config/mongodb-keyfile/${REPLSET_NAME:-rs0} < /mongodb-keyfile
+etcdctl set /mongo-config/mongodb-keyfile/${REPLSET_NAME:-rs0} < ./mongodb-keyfile
+rm ./mongodb-keyfile
