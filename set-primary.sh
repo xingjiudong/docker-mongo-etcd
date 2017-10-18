d61dbbbc6f7b4626678eb40716cb28629b644d0f
xingjiudong <25635680@qq.com>
2017-10-18 15:48:05 +0800

    fix set-primary.sh

--- set-primary.sh
+++ set-primary.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 source ./env.config
+CONN=$1
 
 #Get mongodb replset primary node;
-PRIMARY_NODE=` docker-compose ${COMPOSE_OPT} $@ exec rs0node1 bash -c \
+PRIMARY_NODE=` docker-compose ${COMPOSE_OPT} exec ${CONN} bash -c \
          "echo 'rs.status().members.find(r=>r.state===1).name;' | mongo  -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin" \
-         | grep rs0` 
+         | sed -n '4,4p'`
 
 #Set etcd for mongodb replset primary node;
 etcdctl set haproxy-config/${PROJECT_NAME}/mongo/backend/mongo-1 ${PRIMARY_NODE}
