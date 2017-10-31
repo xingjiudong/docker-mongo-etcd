a4ce024a4b17b7a0ba9d389bf8e83a1b9595bc84
xingjiudong <25635680@qq.com>
2017-10-31 15:33:17 +0800

    add command

--- etcd-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /mongo-config/mongodb-keyfile/${REPLSET_NAME} > /mongodb-keyfile
-chmod 600 /mongodb-keyfile
-chown mongodb /mongodb-keyfile
-source /usr/local/bin/docker-entrypoint.sh
