c49bc562df20c0f088bb9775c24fab1b5ad4eb3a
xingjiudong <25635680@qq.com>
2017-11-03 17:29:35 +0800

    Fix name

--- setup-etcd.sh
+++ setup-etcd.sh
@@ -6,4 +6,4 @@ source env.config
 etcdctl mkdir /haproxy-config/${PROJECT_NAME}/mongo
 etcdctl set haproxy-config/${PROJECT_NAME}/mongo/frontend/bind *:27017
 etcdctl set haproxy-config/${PROJECT_NAME}/mongo/frontend/default_backend mongo-server
-etcdctl set haproxy-config/${PROJECT_NAME}/mongo/backend/mongo-1 mongos:27017
+etcdctl set haproxy-config/${PROJECT_NAME}/mongo/backend/mongo-1 mongo:27017
